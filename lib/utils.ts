24a0311e214a50c80663bc2927421f27db8c1333
lhs2257 <lhs2257@gmail.com>
2026-01-12 16:53:24 +0900

    Fix build error: Restore missing 'cn' utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function urlBase64ToUint8Array(base64String: string) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4)
   const base64 = (base64String + padding)
